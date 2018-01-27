0fc203e566dfbfa0f1af880bf15caa5cb2fad597
Galahad91 <alex.elia.91@gmail.com>
2018-01-27 13:26:23 +0100

    new enemy script added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public int hp;
+    public int dmg;
+    public float speed;
+    public NavMeshAgent agent;
+    public Transform nexus;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(nexus.position);
+    }
+
+}
--- Assets/Scripts/WebRaycast.cs
+++ Assets/Scripts/WebRaycast.cs
@@ -8,9 +8,10 @@ public class WebRaycast : MonoBehaviour
     public List<GameObject> spiderWebs;
     public GameObject spiderWeb;
     public GameObject cameraMain;
-    GameObject start;
-    GameObject end;
     public int clickCount = 0;
+
+    GameObject end;
+    GameObject start;
 	
 	void Start ()
     {
@@ -18,51 +19,45 @@ public class WebRaycast : MonoBehaviour
         spiderWebs = new List<GameObject>();
     }
 	
-	void Update ()
+    public void AddWebs()
     {
-        //if (webs.Count > 1)
-        //{
-        //    for (int i = 0; i < webs.Count; i++)
-        //    {
-        //        if (i > 0)
-        //        {
-        //            webs[i].buildString();
-        //        }
-        //    }
-        //}
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(cameraMain.transform.position, ray.direction, out hit))
+        {
+            if (hit.transform.tag == "WebJoint")
+            {
+                Debug.Log(hit.transform);
+                if (clickCount == 0)
+                {
+                    start = hit.transform.gameObject;
+                    Debug.Log("click 0");
+                }
+                else if (clickCount == 1)
+                {
+                    end = hit.transform.gameObject;
+                    SpiderString webString = new SpiderString(start, end);
+                    webs.Add(webString);
+                    Debug.Log("click 1");
+                }
+                else
+                {
+                    start = end;
+                    end = hit.transform.gameObject;
+                    SpiderString webString = new SpiderString(start, end);
+                    webs.Add(webString);
+                    Debug.Log("click 2+");
+                }
+                clickCount++;
+            }
+        }
+    }
 
+	void Update ()
+    {       
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(cameraMain.transform.position,ray.direction, out hit))
-            {
-                if (hit.transform.tag == "WebJoint")
-                {
-                    Debug.Log(hit.transform);
-                    if (clickCount == 0)
-                    {
-                        start = hit.transform.gameObject;
-                        Debug.Log("click 0");
-                    }
-                    else if(clickCount == 1)
-                    {
-                        end = hit.transform.gameObject;
-                        SpiderString webString = new SpiderString(start,end);
-                        webs.Add(webString);
-                        Debug.Log("click 1");
-                    }
-                    else
-                    {
-                        start = end;
-                        end = hit.transform.gameObject;
-                        SpiderString webString = new SpiderString(start, end);
-                        webs.Add(webString);
-                        Debug.Log("click 2+");
-                    }
-                    clickCount++;
-                }          
-            }
+            AddWebs();
         }
 	}
 }
